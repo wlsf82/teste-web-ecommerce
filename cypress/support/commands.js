834d0fa3caf8f8fcb2b4f5a3e9e36810454b9de3
Nathanael Henrique Souza Nunes <nathanhenrique.nunes.souza@gmail.com>
2025-02-25 22:50:20 -0300

    Teste do e-commerce (carrinho e compra realizada com sucesso)

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,8 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('buySuccess', (date) => {
+    cy.get('#name').type(date.name)
+    cy.get('#country').type(date.country)
+    cy.get('#city').type(date.city)
+    cy.get('#card').type(date.creditcard)
+    cy.get('#month').type(date.month)
+    cy.get('#year').type(date.year)
+})
