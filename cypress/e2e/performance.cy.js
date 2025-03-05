09ba21e892125ca32f45240fc60b3bfd3709e4ca
Walmyr <wlsf82@gmail.com>
2025-03-05 20:34:07 +0100

    Add assertion on "performance" test

--- cypress/e2e/performance.cy.js
+++ cypress/e2e/performance.cy.js
@@ -1,5 +1,12 @@
 describe('Performance', () => {
   it('Deve carregar a pagina em menos de 2 segundos', () => {
+    cy.intercept('GET', `${Cypress.env('API_URL')}/entries`)
+      .as('apiRequest')
+
     cy.visit('/', { timeout: 2000 })
+
+    cy.wait('@apiRequest')
+      .its('response.statusCode')
+      .should('be.equal', 200)
   })
 })
