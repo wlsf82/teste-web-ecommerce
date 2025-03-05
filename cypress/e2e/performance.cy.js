6fdba798d35a82b04634a04759890140c20d0448
Walmyr <wlsf82@gmail.com>
2025-03-05 15:59:57 +0100

    Minor code improvement

--- cypress/e2e/performance.cy.js
+++ cypress/e2e/performance.cy.js
@@ -1,5 +1,5 @@
 describe('Teste de Performance - Tempo de Carregamento', () => {
   it('Deve carregar a pagina em menos de 2 segundos', () => {
-    cy.visit('/', {timeout: 2000})
+    cy.visit('/', { timeout: 2000 })
   })
 })
