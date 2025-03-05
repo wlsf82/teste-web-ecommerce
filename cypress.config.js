a3b825e442c8a8e86c35f369d602914534d46eae
Walmyr <wlsf82@gmail.com>
2025-03-05 20:33:57 +0100

    Configure the api url

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,9 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://www.demoblaze.com',
+    env: {
+      API_URL: 'https://api.demoblaze.com'
+    },
     fixturesFolder: false,
   },
 })
