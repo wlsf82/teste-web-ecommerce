d120481309eecb8aeb474757f86e491c9839aad7
Walmyr <wlsf82@gmail.com>
2025-03-06 01:38:12 +0100

    Simplity the add to cart test suite
    There's no point in testing the same thing more than once.

--- cypress/e2e/cart.cy.js
+++ cypress/e2e/cart.cy.js
@@ -1,26 +1,15 @@
 describe('Carrinho de compras', () => {
   beforeEach(() => cy.visit('/'))
 
-  const produtos = [
-    'Sony vaio i5',
-    'Sony vaio i7',
-    'MacBook air',
-    'Dell i7 8gb',
-    '2017 Dell 15.6 Inch',
-    'MacBook Pro',
-  ]
+  it("Deve adicionar o laptop 'MacBook Pro' ao carrinho", () => {
+    const alertShown = cy.stub().as('alertShown')
 
-  produtos.forEach(produto => {
-    it(`Deve adicionar o produto '${produto}' ao carrinho`, () => {
-      const alertShown = cy.stub().as('alertShown')
+    cy.on('window:alert', alertShown)
 
-      cy.on('window:alert', alertShown)
+    cy.contains('a', 'Laptops').click()
+    cy.contains('.card-title', 'MacBook Pro').click()
+    cy.contains('Add to cart', { timeout: 10000 }).click()
 
-      cy.contains('a', 'Laptops').click()
-      cy.contains('.card-title', produto).click()
-      cy.contains('Add to cart', { timeout: 10000 }).click()
-
-      cy.get('@alertShown').should('have.been.calledOnceWith', 'Product added')
-    })
+    cy.get('@alertShown').should('have.been.calledOnceWith', 'Product added')
   })
 })
